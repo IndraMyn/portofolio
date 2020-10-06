f09acb1e9e49141519766a2fbfdd957094df2c5c
Indra Mulyawan <indraawan13@gmail.com>
2020-10-06 18:56:06 +0700

    Updates

---
+++ precache-manifest.450b28855e93bc38994616ff7d46673b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff91ddf95f0dfc418335af38da17f73f",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "bd71c746ba11fe66a386",
+    "url": "/portofolio/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "c7e7e4e0a223360201a4",
+    "url": "/portofolio/static/css/main.81c09a30.chunk.css"
+  },
+  {
+    "revision": "bd71c746ba11fe66a386",
+    "url": "/portofolio/static/js/2.8b590ec7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portofolio/static/js/2.8b590ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f66a7279e272d13089",
+    "url": "/portofolio/static/js/3.93d5400a.chunk.js"
+  },
+  {
+    "revision": "4c044f40e962d9339bd024f646a38d5a",
+    "url": "/portofolio/static/js/3.93d5400a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e411b3baaa3c15e228",
+    "url": "/portofolio/static/js/4.f6cb0755.chunk.js"
+  },
+  {
+    "revision": "ffc1fbc67add0497afc4",
+    "url": "/portofolio/static/js/5.2d517b68.chunk.js"
+  },
+  {
+    "revision": "c7e7e4e0a223360201a4",
+    "url": "/portofolio/static/js/main.f71cc899.chunk.js"
+  },
+  {
+    "revision": "78ca4dcaacbf4717f498",
+    "url": "/portofolio/static/js/runtime-main.5e29eb01.js"
+  },
+  {
+    "revision": "f4e865762b193f58f34725f744df4679",
+    "url": "/portofolio/static/media/absensi_perusahaan.f4e86576.jpeg"
+  },
+  {
+    "revision": "febdbcb331fbd402a8894305d3bc650b",
+    "url": "/portofolio/static/media/absensi_sekolah.febdbcb3.jpeg"
+  },
+  {
+    "revision": "a17a09af57601f972119acc42eda0d7f",
+    "url": "/portofolio/static/media/achievement1.a17a09af.jpeg"
+  },
+  {
+    "revision": "f2065a298ee78691af7147531692462b",
+    "url": "/portofolio/static/media/avatar.f2065a29.jpeg"
+  },
+  {
+    "revision": "22b0852322a29e4b536d69446cec9c29",
+    "url": "/portofolio/static/media/not_found.22b08523.svg"
+  },
+  {
+    "revision": "b77669548c748e7b793ec8440c09caba",
+    "url": "/portofolio/static/media/online_shop.b7766954.jpeg"
+  },
+  {
+    "revision": "d53385ac5accbd1cd95ad70d1f318398",
+    "url": "/portofolio/static/media/profile_details.d53385ac.svg"
+  },
+  {
+    "revision": "b1a664ffc5af80918be6ab71da8fccdb",
+    "url": "/portofolio/static/media/resume.b1a664ff.jpeg"
+  }
+]);
