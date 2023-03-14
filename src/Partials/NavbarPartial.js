8ff2d92155de386dc4d367a297d47a61439f8f36
DESKTOP-089DC2I\user <indra@mythic.xyz>
2023-03-14 11:31:38 +0700

    hotfix: navbar

--- src/Partials/NavbarPartial.js
+++ src/Partials/NavbarPartial.js
@@ -9,14 +9,14 @@ function NavbarPartial(props) {
             <Nav navbar className="navbar-style">
                 <div className="form-inline">
                     <NavItem>
-                        <NavLink className="mr-2" href="#about"><Button color="dark" style={{borderRadius: '20px'}} size="sm" className="btn-outline-blue" outline>About</Button></NavLink>
+                        <NavLink className="mr-2 ml-5" href="#about"><Button color="dark" style={{borderRadius: '20px'}} size="sm" className="btn-outline-blue" outline>About</Button></NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className="mr-2" href="#portofolio"><Button color="dark" style={{borderRadius: '20px'}} size="sm" className="btn-outline-blue" outline>Portofolio</Button></NavLink>
                     </NavItem>
-                    <NavItem>
+                    {/* <NavItem>
                         <NavLink className="mr-2" href="#skill"><Button color="dark" style={{borderRadius: '20px'}} size="sm" className="btn-outline-blue" outline>Skill</Button></NavLink>
-                    </NavItem>
+                    </NavItem> */}
                     <NavItem>
                         <NavLink className="mr-2" href="#contact"><Button color="dark" style={{borderRadius: '20px'}} size="sm" className="btn-outline-blue" outline>Contact</Button></NavLink>
                     </NavItem>
